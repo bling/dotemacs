3921a4f33fb38b2b79580cbbd5f31fed8ed8a646
Bailey Ling <bling@live.ca>
2013-09-08 21:41:50 -0400

    no tabs, show trailing whitespace, matching parens, auto-indent

--- init-editor.el
+++ init-editor.el
@@ -1,5 +1,17 @@
+(when (fboundp 'electric-pair-mode)
+  (setq-default electric-pair-mode 1))
+
+(setq-default
+ indent-tabs-mode nil
+ show-trailing-whitespace t)
+
 (require-package 'undo-tree)
 (global-undo-tree-mode)
 (setq undo-tree-auto-save-history t)
+
+(global-set-key (kbd "RET") 'newline-and-indent)
+
+(require-package 'mic-paren)
+(paren-activate)
 
 (provide 'init-editor)
