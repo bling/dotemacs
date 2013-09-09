c593de23020f9fb687c24ee014f2ab2f23f20d8c
Bailey Ling <bling@live.ca>
2013-09-08 21:42:19 -0400

    add evil-leader, surround, add some maps

--- init-evil.el
+++ init-evil.el
@@ -1,7 +1,26 @@
 (require-package 'evil)
-(setq evil-want-C-u-scroll t)
+(require-package 'evil-leader)
+(require-package 'surround)
 
 (require 'evil)
+(require 'evil-leader)
+(require 'surround)
+
 (evil-mode t)
+(global-evil-leader-mode)
+(global-surround-mode 1)
+
+(setq evil-want-C-u-scroll t)
+
+(evil-leader/set-leader ",")
+(evil-leader/set-key
+  "v" 'split-window-horizontally
+  "s" 'split-window-vertically)
+
+(define-key evil-normal-state-map (kbd "[ b") 'previous-buffer)
+(define-key evil-normal-state-map (kbd "] b") 'next-buffer)
+
+(define-key evil-motion-state-map "j" 'evil-next-visual-line)
+(define-key evil-motion-state-map "k" 'evil-previous-visual-line)
 
 (provide 'init-evil)
