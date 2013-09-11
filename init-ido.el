cf8f89ee737d2b3cd9131e5d908d65b5d59effbc
Bailey Ling <bling@live.ca>
2013-09-11 15:38:50 +0000

    disable ubiquitous for now

--- init-ido.el
+++ init-ido.el
@@ -2,9 +2,10 @@
 (ido-everywhere t)
 (setq ido-enable-flex-matching t)
 
-(when (eval-when-compile (>= emacs-major-version 24))
-  (require-package 'ido-ubiquitous)
-  (ido-ubiquitous-mode t))
+;;; this breaks evil
+;;; (when (eval-when-compile (>= emacs-major-version 24))
+;;;   (require-package 'ido-ubiquitous)
+;;;   (ido-ubiquitous-mode t))
 
 (require-package 'flx-ido)
 (flx-ido-mode t)
