225dd9b749780cee15d5457d19fa147bc007d715
Bailey Ling <bling@live.ca>
2013-09-12 21:50:59 +0000

    re-enable, fixed upstream

--- init-ido.el
+++ init-ido.el
@@ -2,10 +2,9 @@
 (ido-everywhere t)
 (setq ido-enable-flex-matching t)
 
-;;; this breaks evil
-;;; (when (eval-when-compile (>= emacs-major-version 24))
-;;;   (require-package 'ido-ubiquitous)
-;;;   (ido-ubiquitous-mode t))
+(when (eval-when-compile (>= emacs-major-version 24))
+  (require-package 'ido-ubiquitous)
+  (ido-ubiquitous-mode t))
 
 (require-package 'flx-ido)
 (flx-ido-mode t)
