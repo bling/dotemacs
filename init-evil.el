2c65326cbef9ae338b78b377e2ff228b690305d7
Bailey Ling <bling@live.ca>
2013-09-16 19:50:25 -0400

    cursor shape for iterm

--- init-evil.el
+++ init-evil.el
@@ -15,4 +15,12 @@
 (evil-mode t)
 (global-surround-mode 1)
 
+(unless (display-graphic-p)
+  (if (string= (getenv "TERM_PROGRAM") "iTerm.app")
+      (progn
+        (add-hook 'evil-insert-state-entry-hook
+                  (lambda () (send-string-to-terminal "\e]50;CursorShape=1\x7")))
+        (add-hook 'evil-insert-state-exit-hook
+                  (lambda () (send-string-to-terminal "\e]50;CursorShape=0\x7"))))))
+
 (provide 'init-evil)
