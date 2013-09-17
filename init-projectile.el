11de14882d89a9097875b030fca5c6ce8f272467
Bailey Ling <bling@live.ca>
2013-09-16 20:28:57 -0400

    tweak projectile files

--- init-projectile.el
+++ init-projectile.el
@@ -2,4 +2,7 @@
 (require 'projectile)
 (projectile-global-mode)
 
+(setq projectile-cache-file (expand-file-name ".cache/projectile.cache" user-emacs-directory))
+(setq projectile-known-projects-file (expand-file-name ".cache/projectile-bookmarks.eld" user-emacs-directory))
+
 (provide 'init-projectile)
