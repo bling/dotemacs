b71c08da9699b17b096102a6dcc918a4c0630604
Bailey Ling <bling@live.ca>
2013-09-16 20:27:59 -0400

    tweak ac

--- init-auto-complete.el
+++ init-auto-complete.el
@@ -2,4 +2,14 @@
 (require 'auto-complete-config)
 (global-auto-complete-mode t)
 
+(setq
+ ac-auto-show-menu 0.01
+ ac-auto-start 2
+ ac-comphist-file (expand-file-name ".cache/ac-comphist.dat" user-emacs-directory)
+ ac-delay 0.01
+ ac-quick-help-delay 0.5
+ ac-use-fuzzy t)
+
+(add-to-list 'ac-modes 'vimrc-mode)
+
 (provide 'init-auto-complete)
