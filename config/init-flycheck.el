0aa54dd9347cbf7b3fa203212ced12a0ebd756b0
Bailey Ling <bling@live.ca>
2013-09-17 21:45:22 +0000

    disable checkdoc

--- config/init-flycheck.el
+++ config/init-flycheck.el
@@ -2,4 +2,7 @@
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
+(eval-after-load 'flycheck
+  '(setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers)))
+
 (provide 'init-flycheck)
