9a0e866fe7db233fac2479bc8695031dcb1397e9
Bailey Ling <bling@live.ca>
2013-09-20 21:47:45 +0000

    use default ac config, which sets up sources correctly

--- config/init-auto-complete.el
+++ config/init-auto-complete.el
@@ -1,11 +1,11 @@
 (require-package 'auto-complete)
 (require 'auto-complete-config)
-(global-auto-complete-mode t)
+(ac-config-default)
 
 (setq
  ac-auto-show-menu 0.01
  ac-auto-start 2
- ac-comphist-file (expand-file-name ".cache/ac-comphist.dat" user-emacs-directory)
+ ac-comphist-file (concat user-emacs-directory ".cache/ac-comphist.dat")
  ac-delay 0.01
  ac-quick-help-delay 0.5
  ac-use-fuzzy t
