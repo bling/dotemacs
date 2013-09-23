845ec7d7c4de34cb69342b569bd6468fc279fd52
Bailey Ling <bling@live.ca>
2013-09-22 21:26:55 -0400

    enable projectile in non-projects

--- config/init-projectile.el
+++ config/init-projectile.el
@@ -4,6 +4,7 @@
 (projectile-global-mode)
 
 (setq projectile-cache-file (concat user-emacs-directory ".cache/projectile.cache")
-      projectile-known-projects-file (concat user-emacs-directory ".cache/projectile-bookmarks.eld"))
+      projectile-known-projects-file (concat user-emacs-directory ".cache/projectile-bookmarks.eld")
+      projectile-require-project-root nil)
 
 (provide 'init-projectile)
