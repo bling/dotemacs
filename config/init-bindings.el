4caebb72933e4c5aaaa42c5dcce2f115111a7739
Bailey Ling <bling@live.ca>
2013-09-24 19:10:05 -0400

    check if available

--- config/init-bindings.el
+++ config/init-bindings.el
@@ -4,50 +4,57 @@
 (setq guide-key/recursive-key-sequence-flag t)
 (guide-key-mode 1)
 
-(require-package 'key-chord)
-(key-chord-mode 1)
-(key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
-(key-chord-define evil-insert-state-map "kj" 'evil-normal-state)
-
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "C-x C-m") 'smex)
 (global-set-key (kbd "C-c C-m") 'smex)
 (global-set-key (kbd "C-c h") 'helm-mini)
 
-(evil-leader/set-leader ",")
-(evil-leader/set-key
-  "e" 'eval-last-sexp
-  "E" 'eval-defun
-  "b d" 'kill-this-buffer
-  "v" (kbd "C-w v C-w l")
-  "s" (kbd "C-w s C-w j")
-  "g s" 'magit-status
-  "h" help-map
-  "h h" 'help-for-help-internal)
+(when (featurep 'evil-leader)
+  (evil-leader/set-leader ",")
+  (evil-leader/set-key
+    "e" 'eval-last-sexp
+    "E" 'eval-defun
+    "b d" 'kill-this-buffer
+    "v" (kbd "C-w v C-w l")
+    "s" (kbd "C-w s C-w j")
+    "g s" 'magit-status
+    "h" help-map
+    "h h" 'help-for-help-internal))
 
-(define-key evil-normal-state-map (kbd "[ b") 'previous-buffer)
-(define-key evil-normal-state-map (kbd "] b") 'next-buffer)
-(define-key evil-normal-state-map (kbd "C-p") 'projectile-find-file)
-(define-key evil-normal-state-map (kbd "C-q") 'universal-argument)
+(when (featurep 'evil)
+  (require-package 'key-chord)
+  (key-chord-mode 1)
+  (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
+  (key-chord-define evil-insert-state-map "kj" 'evil-normal-state)
 
-(define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
-(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
-(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
-(define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+  (define-key evil-normal-state-map (kbd "[ b") 'previous-buffer)
+  (define-key evil-normal-state-map (kbd "] b") 'next-buffer)
+  (define-key evil-normal-state-map (kbd "C-p") 'projectile-find-file)
+  (define-key evil-normal-state-map (kbd "C-q") 'universal-argument)
 
-(define-key evil-normal-state-map (kbd "SPC /") 'projectile-ack)
+  (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+  (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+  (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+  (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
 
-(define-key evil-motion-state-map "j" 'evil-next-visual-line)
-(define-key evil-motion-state-map "k" 'evil-previous-visual-line)
+  (define-key evil-normal-state-map (kbd "SPC /") 'projectile-ack)
 
-(define-key evil-normal-state-map (kbd "Q") 'my-window-killer)
-(define-key evil-normal-state-map (kbd "Y") (kbd "y$"))
+  (define-key evil-motion-state-map "j" 'evil-next-visual-line)
+  (define-key evil-motion-state-map "k" 'evil-previous-visual-line)
 
-(define-key evil-insert-state-map (kbd "RET") 'evil-ret-and-indent)
-(evil-define-key 'insert eshell-mode-map (kbd "RET") 'eshell-send-input)
+  (define-key evil-normal-state-map (kbd "Q") 'my-window-killer)
+  (define-key evil-normal-state-map (kbd "Y") (kbd "y$"))
 
-(define-key evil-visual-state-map (kbd "*") 'my-visualstar-forward)
-(define-key evil-visual-state-map (kbd "#") 'my-visualstar-backward)
+  (define-key evil-insert-state-map (kbd "RET") 'evil-ret-and-indent)
+  (evil-define-key 'insert eshell-mode-map (kbd "RET") 'eshell-send-input)
+
+  (define-key evil-visual-state-map (kbd "*") 'my-visualstar-forward)
+  (define-key evil-visual-state-map (kbd "#") 'my-visualstar-backward)
+
+  ;; butter fingers
+  (evil-ex-define-cmd "Q" 'evil-quit)
+  (evil-ex-define-cmd "Qa" 'evil-quit-all)
+  (evil-ex-define-cmd "QA" 'evil-quit-all))
 
 ;; escape minibuffer
 (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
@@ -59,11 +66,6 @@
 ;; auto-complete
 (define-key ac-completing-map (kbd "C-n") 'ac-next)
 (define-key ac-completing-map (kbd "C-p") 'ac-previous)
-
-;; butter fingers
-(evil-ex-define-cmd "Q" 'evil-quit)
-(evil-ex-define-cmd "Qa" 'evil-quit-all)
-(evil-ex-define-cmd "QA" 'evil-quit-all)
 
 ;; mouse scrolling in terminal
 (unless (display-graphic-p)
