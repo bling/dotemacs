fca073ac924a294c13f5a7e89622c4fec7d168ea
Bailey Ling <bling@live.ca>
2013-09-26 22:03:23 +0000

    ignore elpa and .cache

--- config/init-projectile.el
+++ config/init-projectile.el
@@ -6,6 +6,9 @@
 (setq projectile-cache-file (concat user-emacs-directory ".cache/projectile.cache")
       projectile-known-projects-file (concat user-emacs-directory ".cache/projectile-bookmarks.eld"))
 
+(add-to-list 'projectile-globally-ignored-directories "elpa")
+(add-to-list 'projectile-globally-ignored-directories ".cache")
+
 (projectile-global-mode t)
 
 (provide 'init-projectile)
