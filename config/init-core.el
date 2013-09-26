fe24d03e7fbe7f7684e518ce3b6abcbe13435a31
Bailey Ling <bling@live.ca>
2013-09-26 00:23:40 -0400

    fix cursor change in daemon mode for real

--- config/init-core.el
+++ config/init-core.el
@@ -1,7 +1,7 @@
 ;;; configuration for things included in the default Emacs distribution
 
-(if (fboundp 'tool-bar-mode)
-    (tool-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 (unless (display-graphic-p)
   (menu-bar-mode -1))
