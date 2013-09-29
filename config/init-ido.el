048c8d985452d056748d5d5aaa5abaa5a2f14bc1
Bailey Ling <bling@live.ca>
2013-09-29 18:23:23 -0400

    tweak ido settings

--- config/init-ido.el
+++ config/init-ido.el
@@ -1,9 +1,14 @@
+(require 'ido)
+
+(setq ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-create-new-buffer 'prompt
+      ido-use-faces nil ;; disabled so we can see flx highlights
+      ido-use-filename-at-point 'guess
+      ido-save-directory-list-file (concat user-emacs-directory ".cache/ido.last"))
+
 (ido-mode t)
 (ido-everywhere t)
-
-(setq ido-enable-flex-matching t
-      ido-use-faces nil ;; disabled so we can see flx highlights
-      ido-save-directory-list-file (concat user-emacs-directory ".cache/ido.last"))
 
 (require-package 'ido-ubiquitous)
 (ido-ubiquitous-mode t)
@@ -15,6 +20,8 @@
 (ido-vertical-mode)
 
 (require-package 'smex)
+(require 'smex)
 (setq smex-save-file (concat user-emacs-directory ".cache/smex-items"))
+(smex-initialize)
 
 (provide 'init-ido)
