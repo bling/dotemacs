52eb546922c57ee607213068eecaf337664d910d
Bailey Ling <bling@live.ca>
2013-11-13 21:42:46 -0500

    fix js snippets

--- config/init-web.el
+++ config/init-web.el
@@ -1,8 +1,8 @@
-(setq js2-highlight-level 3)
-(setq js2-basic-offset 2)
-(setq js2-idle-timer-delay 1)
-
 (require-package 'js2-mode)
+(require 'js2-mode)
+(setq js2-highlight-level 3)
+(setq-default js2-basic-offset 2)
+
 (require-package 'ac-js2)
 (after 'js2-mode-autoloads
   (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
