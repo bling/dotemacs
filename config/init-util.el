046f3997c89303b49e59cf65457e2656c8fd24c0
Bailey Ling <bling@live.ca>
2013-11-23 22:25:46 -0500

    add easy rename/delete buffer file

--- config/init-util.el
+++ config/init-util.el
@@ -60,6 +60,38 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
            (insert (current-kill 0)))))
 
 
+(defun my-rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
+
+
+(defun my-delete-current-buffer-file ()
+  "Removes file connected to current buffer and kills buffer."
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (buffer (current-buffer))
+        (name (buffer-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (ido-kill-buffer)
+      (when (yes-or-no-p "Are you sure you want to remove this file? ")
+        (delete-file filename)
+        (kill-buffer buffer)
+        (message "File '%s' successfully removed" filename)))))
+
+
 ;; make sure $PATH is set correctly
 (require-package 'exec-path-from-shell)
 (ignore-errors ;; windows
