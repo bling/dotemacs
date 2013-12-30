8cddd7a172249e8f0ad7ec60dbc6be663f764f0a
Bailey Ling <bling@live.ca>
2013-12-29 19:53:30 -0500

    regex by default

--- config/init-bindings.el
+++ config/init-bindings.el
@@ -238,6 +238,10 @@
 (global-set-key (kbd "C-x C-k") 'kill-this-buffer)
 (global-set-key (kbd "C-c e") 'my-eval-and-replace)
 
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
 
 ;; have no use for these default bindings
 (global-unset-key (kbd "C-x m"))
