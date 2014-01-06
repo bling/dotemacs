d9705f5f15606bd7d87004687835ae7e5292d1e5
Bailey Ling <bling@live.ca>
2014-01-06 13:34:31 -0500

    use ack for generic command if available

--- config/init-projectile.el
+++ config/init-projectile.el
@@ -7,6 +7,11 @@
 (add-to-list 'projectile-globally-ignored-directories ".cache")
 (add-to-list 'projectile-globally-ignored-directories "node_modules")
 
+(when (executable-find "ack")
+  (require-package 's)
+  (setq projectile-generic-command
+        (concat "ack -f --print0" (s-join " --ignore-dir=" (cons "" projectile-globally-ignored-directories)))))
+
 (projectile-global-mode t)
 
 (provide 'init-projectile)
