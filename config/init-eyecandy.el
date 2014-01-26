e4513e8fffc675cec594b340e0b25f395b0f97ec
Bailey Ling <bling@live.ca>
2014-01-26 15:51:52 -0500

    remove redundant after macro

--- config/init-eyecandy.el
+++ config/init-eyecandy.el
@@ -17,19 +17,18 @@
 
 
 (require-package 'diminish)
-(after 'diminish-autoloads
-  (diminish 'visual-line-mode)
-  (after 'autopair (diminish 'autopair-mode))
-  (after 'undo-tree (diminish 'undo-tree-mode))
-  (after 'auto-complete (diminish 'auto-complete-mode))
-  (after 'projectile (diminish 'projectile-mode))
-  (after 'yasnippet (diminish 'yas-minor-mode))
-  (after 'guide-key (diminish 'guide-key-mode))
-  (after 'eldoc (diminish 'eldoc-mode))
-  (after 'smartparens (diminish 'smartparens-mode))
-  (after 'company (diminish 'company-mode))
-  (after 'elisp-slime-nav (diminish 'elisp-slime-nav-mode))
-  (after 'git-gutter+ (diminish 'git-gutter+-mode)))
+(diminish 'visual-line-mode)
+(after 'autopair (diminish 'autopair-mode))
+(after 'undo-tree (diminish 'undo-tree-mode))
+(after 'auto-complete (diminish 'auto-complete-mode))
+(after 'projectile (diminish 'projectile-mode))
+(after 'yasnippet (diminish 'yas-minor-mode))
+(after 'guide-key (diminish 'guide-key-mode))
+(after 'eldoc (diminish 'eldoc-mode))
+(after 'smartparens (diminish 'smartparens-mode))
+(after 'company (diminish 'company-mode))
+(after 'elisp-slime-nav (diminish 'elisp-slime-nav-mode))
+(after 'git-gutter+ (diminish 'git-gutter+-mode))
 
 
 (require 'linum)
