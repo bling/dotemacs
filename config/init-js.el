7d37c24b97b387b9da18af3f6da8cf1864049f47
Bailey Ling <bling@live.ca>
2014-01-26 14:55:30 -0500

    always add js2-mode to mode list, since it's already required anyways. resolves #4.

--- config/init-js.el
+++ config/init-js.el
@@ -2,11 +2,11 @@
 (require 'js2-mode)
 (setq js2-highlight-level 3)
 (setq-default js2-basic-offset 2)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 
-(require-package 'ac-js2)
-(after 'js2-mode-autoloads
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
+(after 'auto-complete-autoloads
+  (require-package 'ac-js2))
 
 
 (require-package 'js2-refactor)
