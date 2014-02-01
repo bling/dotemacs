1a429931db34ac4c8bb1887681a00b26a662fce5
Bailey Ling <bling@live.ca>
2014-01-31 20:19:54 -0500

    upstream fixed, don't need this no more

--- elpa/fringe-helper-20130519.1641/fringe-helper-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; fringe-helper-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("fringe-helper-pkg.el" "fringe-helper.el")
-;;;;;;  (21125 8004 530000 0))
-
-;;;***
-
-(provide 'fringe-helper-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; fringe-helper-autoloads.el ends here
--- elpa/fringe-helper-20130519.1641/fringe-helper-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "fringe-helper" "20130519.1641" "helper functions for fringe bitmaps [github]" 'nil)
