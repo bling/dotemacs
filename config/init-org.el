59e64d0dade553c303f447b95e5f836b5b7b0f0e
Bailey Ling <bling@live.ca>
2014-02-03 09:59:15 -0500

    use org-directory

--- config/init-org.el
+++ config/init-org.el
@@ -1,24 +1,23 @@
-(setq org-default-notes-file "~/.notes.org")
-(setq org-log-done t)
-
-
-(setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/.notes.org" "Tasks")
-         "* TODO %?  %T")
-        ("j" "Journal" entry (file+datetree "~/.journal.org")
-         "* %?\n%U\n")))
-(after 'evil
-  (add-hook 'org-capture-mode-hook (lambda () (evil-insert-state))))
-
-
 (after 'org
+  (setq org-default-notes-file (concat org-directory "/notes.org"))
+  (setq org-log-done t)
+
+
+  (setq org-capture-templates
+        '(("t" "Todo" entry (file+headline (concat org-directory "/notes.org") "Tasks")
+           "* TODO %?  %T")
+          ("j" "Journal" entry (file+datetree (concat org-directory "/journal.org"))
+           "* %?\n%U\n")))
+
+  (after 'evil
+    (add-hook 'org-capture-mode-hook (lambda () (evil-insert-state))))
+
   (require 'ob)
   (org-babel-do-load-languages
    'org-babel-load-languages
-   '((plantuml . t))))
+   '((plantuml . t)))
 
-
-(setq org-plantuml-jar-path (expand-file-name "~/plantuml.jar"))
+  (setq org-plantuml-jar-path (expand-file-name "~/plantuml.jar")))
 
 
 (provide 'init-org)
