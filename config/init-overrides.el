8ba47599b76bc7d20946707ef2f2d8db21cf9a9d
Bailey Ling <bling@live.ca>
2014-03-02 09:43:55 -0500

    fix merged upstream

--- config/init-overrides.el
+++ config/init-overrides.el
@@ -18,28 +18,7 @@
      ;; others : digit , alpha, punc
      (setq ace-jump-query-char query-char1)
      (setq ace-jump-current-mode 'ace-jump-char-mode)
-     (ace-jump-do (regexp-quote (string query-char1 query-char2))))
+     (ace-jump-do (regexp-quote (string query-char1 query-char2))))))
 
-
-   ;; fixes git-grep usage in windows by replacing "'*'" with ""
-   (defun projectile-grep ()
-     "Perform rgrep in the project."
-     (interactive)
-     (let ((roots (projectile-get-project-directories))
-           (search-regexp (if (and transient-mark-mode mark-active)
-                              (buffer-substring (region-beginning) (region-end))
-                            (read-string (projectile-prepend-project-name "Grep for: ")
-                                         (projectile-symbol-at-point)))))
-       (dolist (root-dir roots)
-         (require 'grep)
-         ;; in git projects users have the option to use `vc-git-grep' instead of `rgrep'
-         (if (and (eq (projectile-project-vcs) 'git) projectile-use-git-grep)
-             (vc-git-grep search-regexp "" root-dir)
-           ;; paths for find-grep should relative and without trailing /
-           (let ((grep-find-ignored-directories (-union (-map (lambda (dir) (s-chop-suffix "/" (file-relative-name dir root-dir)))
-                                                              (cdr (projectile-ignored-directories))) grep-find-ignored-directories))
-                 (grep-find-ignored-files (-union (-map (lambda (file) (file-relative-name file root-dir)) (projectile-ignored-files)) grep-find-ignored-files)))
-             (grep-compute-defaults)
-             (rgrep search-regexp "* .*" root-dir))))))))
 
 (provide 'init-overrides)
