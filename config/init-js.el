9b3f839449b0fb6e04c0809daccf8feae17bb8de
Bailey Ling <bling@live.ca>
2014-03-08 12:17:11 -0500

    fix ac-js2 and js2-refactor

--- config/init-js.el
+++ config/init-js.el
@@ -1,14 +1,18 @@
 (lazy-major-mode "\\.js$" js2-mode)
 
 (after 'js2-mode
+
   (require-package 'ac-js2)
+  (add-hook 'js2-mode-hook 'ac-js2-mode)
+
   (require-package 'js2-refactor)
+  (js2r-add-keybindings-with-prefix "C-c C-m")
 
   (setq js2-highlight-level 3)
   (setq-default js2-basic-offset 2)
 
   (require-package 'tern)
-  (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+  (add-hook 'js2-mode-hook 'tern-mode)
   (after 'tern
     (after 'auto-complete
       (require-package 'tern-auto-complete)
