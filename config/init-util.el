540bc9b47ec7bd5ccce0eb1fbb34cc68c3939b9a
Bailey Ling <bling@live.ca>
2014-03-09 10:43:57 -0400

    use with-eval-after-load for 24.4

--- config/init-util.el
+++ config/init-util.el
@@ -1,8 +1,13 @@
-(defmacro after (feature &rest body)
-  "After FEATURE is loaded, evaluate BODY."
-  (declare (indent defun))
-  `(eval-after-load ,feature
-     '(progn ,@body)))
+(if (fboundp 'with-eval-after-load)
+    (defmacro after (feature &rest body)
+      "After FEATURE is loaded, evaluate BODY."
+      (declare (indent defun))
+      `(with-eval-after-load ,feature ,@body))
+  (defmacro after (feature &rest body)
+    "After FEATURE is loaded, evaluate BODY."
+    (declare (indent defun))
+    `(eval-after-load ,feature
+       '(progn ,@body))))
 
 
 (defmacro lazy-major-mode (pattern mode)
