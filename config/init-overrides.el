e3606b5a30e27a002ec2180e61eee1402b52287b
Bailey Ling <bling@live.ca>
2014-03-11 21:36:39 -0400

    redefine after mode is loaded

--- config/init-overrides.el
+++ config/init-overrides.el
@@ -2,23 +2,24 @@
  'after-init-hook
  (lambda ()
    ;; always 2 char jumping
-   (defun ace-jump-char-mode (query-char1 query-char2)
-     "AceJump char mode"
-     (interactive (list (read-char "Query Char (1/2):") (read-char "Query Char (2/2):")))
+   (after 'ace-jump-mode
+     (defun ace-jump-char-mode (query-char1 query-char2)
+       "AceJump char mode"
+       (interactive (list (read-char "Query Char (1/2):") (read-char "Query Char (2/2):")))
 
-     ;; We should prevent recursion call this function.  This can happen
-     ;; when you trigger the key for ace jump again when already in ace
-     ;; jump mode.  So we stop the previous one first.
-     (if ace-jump-current-mode (ace-jump-done))
+       ;; We should prevent recursion call this function.  This can happen
+       ;; when you trigger the key for ace jump again when already in ace
+       ;; jump mode.  So we stop the previous one first.
+       (if ace-jump-current-mode (ace-jump-done))
 
-     (if (or (eq (ace-jump-char-category query-char1) 'other)
-             (eq (ace-jump-char-category query-char2) 'other))
-         (error "[AceJump] Non-printable character"))
+       (if (or (eq (ace-jump-char-category query-char1) 'other)
+               (eq (ace-jump-char-category query-char2) 'other))
+           (error "[AceJump] Non-printable character"))
 
-     ;; others : digit , alpha, punc
-     (setq ace-jump-query-char query-char1)
-     (setq ace-jump-current-mode 'ace-jump-char-mode)
-     (ace-jump-do (regexp-quote (string query-char1 query-char2))))))
+       ;; others : digit , alpha, punc
+       (setq ace-jump-query-char query-char1)
+       (setq ace-jump-current-mode 'ace-jump-char-mode)
+       (ace-jump-do (regexp-quote (string query-char1 query-char2)))))))
 
 
 (provide 'init-overrides)
