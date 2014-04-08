979024a42643ccc85d972d5c57f8f41946d054fc
Bailey Ling <bling@live.ca>
2014-04-08 15:08:35 -0400

    exclude terminals from company, don't complete with space, immediate suggestions

--- config/init-company.el
+++ config/init-company.el
@@ -3,10 +3,11 @@
 
 (setq company-auto-complete t)
 (setq company-global-modes t)
-(setq company-idle-delay 0.2)
+(setq company-idle-delay t)
 (setq company-minimum-prefix-length 1)
 (setq company-show-numbers t)
 (setq company-tooltip-limit 30)
+(setq company-auto-complete-chars '(41 46))
 
 (after "ac-js2-autoloads"
   (add-to-list 'company-backends 'ac-js2-company))
@@ -14,6 +15,10 @@
 (when (executable-find "tern")
   (after "company-tern-autoloads"
     (add-to-list 'company-backends 'company-tern)))
+
+(setq company-global-modes
+      '(not
+        eshell-mode shell-mode term-mode terminal-mode))
 
 (global-company-mode t)
 
