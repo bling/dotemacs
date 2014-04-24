6acba56a6d0d148414f352de11b802979859b225
Bailey Ling <bling@live.ca>
2014-04-24 00:31:06 -0400

    improve org usage

--- config/init-bindings.el
+++ config/init-bindings.el
@@ -245,6 +245,7 @@
 
 (global-set-key (kbd "C-c c") 'org-capture)
 (global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c s") 'my-goto-scratch-buffer)
 
 (global-set-key (kbd "C-x C-b") 'ibuffer)
--- config/init-org.el
+++ config/init-org.el
@@ -5,12 +5,34 @@
   (setq org-default-notes-file (concat org-directory "/notes.org"))
   (setq org-log-done t)
 
+  (setq org-startup-indented t)
   (setq org-agenda-files `(,org-directory))
   (setq org-capture-templates
-        '(("t" "Todo" entry (file+headline (concat org-directory "/notes.org") "Tasks")
-           "* TODO %?  %T")
+        '(("t" "Todo" entry (file (concat org-directory "/notes.org"))
+           "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+          ("n" "Note" entry (file (concat org-directory "/notes.org"))
+           "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+          ("m" "Metting" entry (file (concat org-directory "/notes.org"))
+           "* MEETING %? :MEETING:\n%U" :clock-in t :clock-resume t)
           ("j" "Journal" entry (file+datetree (concat org-directory "/journal.org"))
-           "* %?\n%U\n")))
+           "* %?\n%U\n" :clock-in t :clock-resume t)))
+
+  (setq org-use-fast-todo-selection t)
+  (setq org-treat-S-cursor-todo-selection-as-state-change nil)
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+          (sequence "BLOCKED(b@/!)" "|" "CANCELLED(c@/!)")))
+
+  (setq org-todo-state-tags-triggers
+        ' (("CANCELLED" ("CANCELLED" . t))
+           ("BLOCKED" ("BLOCKED" . t))
+           ("TODO" ("BLOCKED") ("CANCELLED"))
+           ("NEXT" ("BLOCKED") ("CANCELLED"))
+           ("DONE" ("BLOCKED") ("CANCELLED"))))
+
+  (setq org-refile-targets '((nil :maxlevel . 9)
+                             (org-agenda-files :maxlevel . 9)))
+  (setq org-completion-use-ido t)
 
   (after 'evil
     (add-hook 'org-capture-mode-hook 'evil-insert-state))
