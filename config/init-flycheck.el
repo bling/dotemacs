deb36770e9eefa9976209870f603157c596c961a
Bailey Ling <bling@live.ca>
2014-06-21 12:30:00 -0400

    disable next-error override from flycheck

--- config/init-flycheck.el
+++ config/init-flycheck.el
@@ -3,7 +3,8 @@
 (after 'flycheck
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
   (setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers))
-  (setq flycheck-checkers (delq 'html-tidy flycheck-checkers)))
+  (setq flycheck-checkers (delq 'html-tidy flycheck-checkers))
+  (setq flycheck-standard-error-navigation nil))
 
 (global-flycheck-mode t)
 
