c7c9e6be53b605c89b91b545489fa81f4c7f6c16
Bailey Ling <bling@live.ca>
2014-10-27 11:20:14 -0400

    group settings with packages

--- config/init-evil.el
+++ config/init-evil.el
@@ -1,36 +1,3 @@
-(setq evil-search-module 'evil-search)
-(setq evil-magic 'very-magic)
-
-(setq evil-emacs-state-cursor '("red" box))
-(setq evil-normal-state-cursor '("green" box))
-(setq evil-visual-state-cursor '("orange" box))
-(setq evil-insert-state-cursor '("red" bar))
-(setq evil-replace-state-cursor '("red" bar))
-(setq evil-operator-state-cursor '("red" hollow))
-
-(setq evilnc-hotkey-comment-operator "gc")
-
-(setq evil-jumper-auto-center t)
-(setq evil-jumper-file (concat dotemacs-cache-directory "evil-jumps"))
-(setq evil-jumper-auto-save-interval 3600)
-
-(require-package 'evil)
-(require-package 'evil-leader)
-(require-package 'evil-numbers)
-(require-package 'evil-visualstar)
-(require-package 'evil-nerd-commenter)
-(require-package 'evil-indent-textobject)
-(require-package 'evil-matchit)
-(require-package 'evil-exchange)
-(require-package 'evil-surround)
-(require-package 'evil-jumper)
-
-(require 'evil)
-(require 'evil-nerd-commenter)
-(require 'evil-indent-textobject)
-(require 'evil-visualstar)
-(require 'evil-jumper)
-
 (defcustom dotemacs-evil-state-modes
   '(fundamental-mode
     text-mode
@@ -44,20 +11,68 @@
   :type '(repeat (symbol))
   :group 'dotemacs)
 
+
+(setq evil-search-module 'evil-search)
+(setq evil-magic 'very-magic)
+
+(setq evil-emacs-state-cursor '("red" box))
+(setq evil-normal-state-cursor '("green" box))
+(setq evil-visual-state-cursor '("orange" box))
+(setq evil-insert-state-cursor '("red" bar))
+(setq evil-replace-state-cursor '("red" bar))
+(setq evil-operator-state-cursor '("red" hollow))
+
+(require-package 'evil)
+(require 'evil)
+
+
+(require-package 'evil-leader)
+(global-evil-leader-mode t)
+
+
+(setq evilnc-hotkey-comment-operator "gc")
+(require-package 'evil-nerd-commenter)
+(require 'evil-nerd-commenter)
+
+
+(require-package 'evil-surround)
+(global-evil-surround-mode t)
+
+
+(require-package 'evil-exchange)
+(evil-exchange-install)
+
+
+(setq evil-jumper-auto-center t)
+(setq evil-jumper-file (concat dotemacs-cache-directory "evil-jumps"))
+(setq evil-jumper-auto-save-interval 3600)
+(require-package 'evil-jumper)
+(require 'evil-jumper)
+
+
+(require-package 'evil-matchit)
+(defun evilmi-customize-keybinding ()
+  (evil-define-key 'normal evil-matchit-mode-map
+    "%" 'evilmi-jump-items))
+(global-evil-matchit-mode t)
+
+
+(require-package 'evil-indent-textobject)
+(require 'evil-indent-textobject)
+
+
+(require-package 'evil-visualstar)
+(require 'evil-visualstar)
+
+
+(require-package 'evil-numbers)
+
+
 (defun my-enable-evil-mode ()
   (if (apply 'derived-mode-p dotemacs-evil-state-modes)
       (turn-on-evil-mode)
     (set-cursor-color "red")))
 (add-hook 'after-change-major-mode-hook 'my-enable-evil-mode)
-
-(global-evil-leader-mode t)
-(global-evil-surround-mode t)
-(evil-exchange-install)
-
-(defun evilmi-customize-keybinding ()
-  (evil-define-key 'normal evil-matchit-mode-map
-    "%" 'evilmi-jump-items))
-(global-evil-matchit-mode t)
 
 (defun my-send-string-to-terminal (string)
   (unless (display-graphic-p) (send-string-to-terminal string)))
