c58d61d649a4ea15a933daaba3902d8b05afe29f
Bailey Ling <bling@live.ca>
2014-12-23 16:48:39 -0800

    save excursion

--- config/init-macros.el
+++ config/init-macros.el
@@ -4,12 +4,13 @@
 (defun my-macro-ng-add-string-for-last-arg ()
   "With the cursor on the last argument of a function, this will extract the name
 of the argument and insert a string with the value value prior to the function."
-  (interactive)
-  (evil-execute-macro 1 [?F ?, ?w ?y ?w ?F ?\( ?b ?b ?l ?a ?  ?\' escape ?p ?l ?a ?, escape]))
+  (save-excursion
+    (evil-execute-macro 1 [?F ?, ?w ?y ?w ?F ?\( ?b ?b ?l ?a ?  ?\' escape ?p ?l ?a ?, escape])))
 
 (defun my-macro-ng-function-to-array-injected ()
   "With the cursor inside the parameter list of a function, wraps the function inside
 an array and adds a string for the first argument in the parameter list."
-  (evil-execute-macro 1 [?F ?\( ?l ?y ?w ?F ?f ?i ?\[ ?\' escape ?p ?l ?l ?c ?w ?, ?  escape ?f ?\{ ?% ?a ?\] escape]))
+  (save-excursion
+    (evil-execute-macro 1 [?F ?\( ?l ?y ?w ?F ?f ?i ?\[ ?\' escape ?p ?l ?l ?c ?w ?, ?  escape ?f ?\{ ?% ?a ?\] escape])))
 
 (provide 'init-macros)
