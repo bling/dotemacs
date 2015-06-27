e255d60507f3d8bf4ce17e51da0f309f9f6eb728
Bailey Ling <bling@live.ca>
2015-06-26 20:08:23 -0400

    remap describe functions to counsel

--- config/init-ivy.el
+++ config/init-ivy.el
@@ -10,6 +10,8 @@
 (when (eq dotemacs-switch-engine 'ivy)
   (ivy-mode t)
   (global-set-key [remap execute-extended-command] #'counsel-M-x)
+  (global-set-key [remap describe-function] #'counsel-describe-function)
+  (global-set-key [remap describe-variable] #'counsel-describe-variable)
   (global-set-key [remap find-file] #'counsel-find-file))
 
 (provide 'init-ivy)
