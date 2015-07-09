e0017f97520c55081a80a28591149c25a8400267
Bailey Ling <bling@live.ca>
2015-07-08 23:05:52 -0400

    butter fingers

--- config/init-bindings.el
+++ config/init-bindings.el
@@ -102,6 +102,10 @@
   (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
   (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
   (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+  (define-key evil-normal-state-map (kbd "C-w C-h") 'evil-window-left)
+  (define-key evil-normal-state-map (kbd "C-w C-j") 'evil-window-down)
+  (define-key evil-normal-state-map (kbd "C-w C-k") 'evil-window-up)
+  (define-key evil-normal-state-map (kbd "C-w C-l") 'evil-window-right)
 
   (define-key evil-motion-state-map "j" 'evil-next-visual-line)
   (define-key evil-motion-state-map "k" 'evil-previous-visual-line)
