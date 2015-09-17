9fd63c44f21e5c43a64fb48d729b3df4442a3b98
Bailey Ling <bling@live.ca>
2015-09-17 10:35:49 -0400

    map projectile's completion engine to the same one in dotemacs

--- config/init-projectile.el
+++ config/init-projectile.el
@@ -1,23 +1,20 @@
 (require-package 'projectile)
 
-
 (setq projectile-cache-file (concat dotemacs-cache-directory "projectile.cache"))
 (setq projectile-known-projects-file (concat dotemacs-cache-directory "projectile-bookmarks.eld"))
 (setq projectile-indexing-method 'alien)
 (setq projectile-enable-caching t)
+(setq projectile-completion-system dotemacs-switch-engine)
+
 (after "helm-autoloads"
-  (setq projectile-completion-system 'helm)
   (after 'helm-projectile
     (add-to-list 'helm-projectile-sources-list 'helm-source-projectile-recentf-list)))
 
-
 (require 'projectile)
-
 
 (add-to-list 'projectile-globally-ignored-directories "elpa")
 (add-to-list 'projectile-globally-ignored-directories ".cache")
 (add-to-list 'projectile-globally-ignored-directories "node_modules")
-
 
 (when (executable-find "ack")
   (require-package 's)
@@ -26,8 +23,6 @@
     (setq projectile-generic-command val)
     (setq projectile-svn-command val)))
 
-
 (projectile-global-mode t)
-
 
 (provide 'init-projectile)
