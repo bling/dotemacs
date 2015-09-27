75b516a46c0f529a902583e1a2ef32211bdf9e45
Bailey Ling <bling@live.ca>
2015-09-27 17:40:53 -0400

    prevent double pair activation

--- config/init-smartparens.el
+++ config/init-smartparens.el
@@ -1,17 +1,17 @@
 (require-package 'smartparens)
 (require 'smartparens-config)
 
-(setq sp-autoinsert-quote-if-followed-by-closing-pair nil)
-
 (when (eq dotemacs-pair-engine 'smartparens)
+  (setq sp-autoinsert-quote-if-followed-by-closing-pair nil)
   (setq sp-autoinsert-pair t)
 
   (setq sp-show-pair-delay 0)
   (setq sp-show-pair-from-inside t)
-  (show-smartparens-global-mode t))
+
+  (show-smartparens-global-mode t)
+  (smartparens-global-mode t))
 
 (sp-use-smartparens-bindings)
-(smartparens-global-mode t)
 
 ;; fix conflict where smartparens clobbers yas' key bindings
 (after 'yasnippet
