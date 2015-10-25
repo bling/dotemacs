eaeda2263639c06bafacd386d17ba3359400df68
Bailey Ling <bling@live.ca>
2015-10-25 11:11:01 -0400

    only go full screen for magit status

--- config/init-vcs.el
+++ config/init-vcs.el
@@ -3,7 +3,11 @@
 (after 'vc-git
   (require-package 'magit)
 
-  (add-hook 'magit-post-display-buffer-hook #'delete-other-windows)
+  (defun my-magit-post-display-buffer ()
+    (if (string-match "*magit:" (buffer-name))
+        (delete-other-windows)))
+  (add-hook 'magit-post-display-buffer-hook #'my-magit-post-display-buffer)
+
   (setq magit-section-show-child-count t)
   (setq magit-diff-arguments '("--histogram"))
   (setq magit-push-always-verify nil)
