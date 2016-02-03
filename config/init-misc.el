814f7e6d0bff5a4ed0d334d222eecbcdab93242d
Bailey Ling <bling@live.ca>
2016-02-02 23:03:09 -0500

    add OS config

--- config/init-misc.el
+++ config/init-misc.el
@@ -96,17 +96,4 @@
 (popwin-mode)
 
 
-(when (eq system-type 'darwin)
-  (require-package 'vkill))
-
-
-;; make sure $PATH is set correctly
-(if (eq system-type 'windows-nt)
-    (dolist (path (split-string (getenv "PATH") ";"))
-      (add-to-list 'exec-path (replace-regexp-in-string "\\\\" "/" path)))
-  (progn
-    (require-package 'exec-path-from-shell)
-    (exec-path-from-shell-initialize)))
-
-
 (provide 'init-misc)
---
+++ config/init-os.el
@@ -0,0 +1,15 @@
+
+(if (eq system-type 'windows-nt)
+    (progn
+      (dolist (path (split-string (getenv "PATH") ";"))
+        (add-to-list 'exec-path (replace-regexp-in-string "\\\\" "/" path))))
+  (progn
+    (require-package 'exec-path-from-shell)
+    (exec-path-from-shell-initialize)))
+
+(when (eq system-type 'darwin)
+  (require-package 'reveal-in-osx-finder)
+  (require-package 'vkill))
+
+
+(provide 'init-os)
