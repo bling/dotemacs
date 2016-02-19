e7ab88b2e68e66969c74fb0004b4031c261fcbed
Bailey Ling <bling@live.ca>
2016-02-19 13:37:44 -0500

    autoload is enough

--- config/init-auto-complete.el
+++ config/init-auto-complete.el
@@ -1,8 +1,6 @@
 (when (eq dotemacs-completion-engine 'auto-complete)
 
   (require-package 'auto-complete)
-  (require 'auto-complete)
-  (require 'auto-complete-config)
 
   (setq ac-auto-show-menu t)
   (setq ac-auto-start t)
@@ -11,10 +9,10 @@
   (setq ac-quick-help-height 30)
   (setq ac-show-menu-immediately-on-auto-complete t)
 
+  (ac-config-default)
+
   (dolist (mode '(vimrc-mode html-mode stylus-mode))
     (add-to-list 'ac-modes mode))
-
-  (ac-config-default)
 
   (after 'linum
     (ac-linum-workaround))
