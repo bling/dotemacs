5316e04daa08cff3b3df2927facb2bd8ba658d30
Bailey Ling <bling@live.ca>
2016-11-12 11:46:42 -0500

    add xterm-color

---
+++ config/init-term.el
@@ -0,0 +1,15 @@
+(require-package 'xterm-color)
+(require 'xterm-color)
+
+(setenv "TERM" "xterm-256color")
+
+(add-hook 'comint-preoutput-filter-functions #'xterm-color-filter)
+(setq comint-output-filter-functions (remove #'ansi-color-process-output comint-output-filter-functions))
+
+(after 'esh-mode
+  (add-to-list 'eshell-preoutput-filter-functions #'xterm-color-filter)
+  (add-hook 'eshell-mode-hook
+            (lambda ()
+              (setq xterm-color-preserve-properties t))))
+
+(provide 'init-term)
