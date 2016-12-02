2f6ac4876360ca3644d0ea2566d773c64f81b054
Bailey Ling <Bailey Ling>
2016-12-02 17:45:59 -0500

    set eshell term to 256 color

--- config/init-term.el
+++ config/init-term.el
@@ -1,8 +1,6 @@
 (require-package 'xterm-color)
 (require 'xterm-color)
 
-(setenv "TERM" "xterm-256color")
-
 (add-hook 'comint-preoutput-filter-functions #'xterm-color-filter)
 (setq comint-output-filter-functions (remove #'ansi-color-process-output comint-output-filter-functions))
 
@@ -10,6 +8,7 @@
   (add-to-list 'eshell-preoutput-filter-functions #'xterm-color-filter)
   (add-hook 'eshell-mode-hook
             (lambda ()
+              (setenv "TERM" "xterm-256color")
               (setq xterm-color-preserve-properties t))))
 
 (provide 'init-term)
