7aa22a35895c5746647129fcd5d7bbcf51dc023f
Bailey Ling <bling@live.ca>
2017-09-30 14:35:22 -0400

    add some basic snippets for typescript/react dev

--- config/init-yasnippet.el
+++ config/init-yasnippet.el
@@ -1,11 +1,6 @@
 (delayed-init
  (require-package 'yasnippet)
 
- (let* ((yas-install-dir (car (file-expand-wildcards (concat package-user-dir "/yasnippet-*"))))
-        (dir (concat yas-install-dir "/snippets/js-mode")))
-   (if (file-exists-p dir)
-       (delete-directory dir t)))
-
  (require 'yasnippet)
 
  (setq yas-fallback-behavior 'return-nil)
@@ -15,6 +10,6 @@
  (add-hook 'prog-mode-hook 'yas-minor-mode)
  (add-hook 'html-mode-hook 'yas-minor-mode)
 
- (yas-load-directory (concat user-emacs-directory "/snippets")))
+ (yas-load-directory (concat user-emacs-directory "snippets")))
 
 (provide 'init-yasnippet)
