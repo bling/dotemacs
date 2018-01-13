bf866a8884d55c2c400a33befdcaaf04de7f68b8
Bailey Ling <bling@live.ca>
2018-01-12 22:26:46 -0500

    turn on flycheck error integration
    doesn't seem to conflict with compile results anymore

--- config/config-flycheck.el
+++ config/config-flycheck.el
@@ -1,6 +1,6 @@
 (require-package 'flycheck)
 
-(setq flycheck-standard-error-navigation nil)
+(setq flycheck-standard-error-navigation t)
 (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc html-tidy))
 
 (after 'web-mode
@@ -13,6 +13,8 @@
   (setq flycheck-pos-tip-timeout -1)
   (flycheck-pos-tip-mode))
 
-(evilify flycheck-error-list-mode flycheck-error-list-mode-map)
+(evilify flycheck-error-list-mode flycheck-error-list-mode-map
+  "j" #'flycheck-error-list-next-error
+  "k" #'flycheck-error-list-previous-error)
 
 (provide 'config-flycheck)
