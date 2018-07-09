af2953c7af0673091562a884aabdc4ab59d73450
Bailey Ling <bling@live.ca>
2018-07-09 19:20:20 -0400

    add extra tide bindings

--- config/bindings/tide.el
+++ config/bindings/tide.el
@@ -1,23 +1,27 @@
 (after [evil tide hydra]
   (defhydra /bindings/tide/hydra (:hint nil :exit t)
     "
-   typescript:  _d_ jump to definition   _R_ refactor             _e_ project errors
-                _h_ documentation        _j_ insert jsdoc         _o_ organize imports
-                _r_ find references      _f_ fix error at point   _v_ verify setup
-                _n_ rename               _F_ format               _S_ restart server
+   typescript:  _d_ jump to definition   _R_ refactor           _e_ project errors       _v_ verify setup
+                _h_ documentation        _j_ insert jsdoc       _o_ organize imports     _S_ restart server
+                _r_ find references      _f_ fix at point       _a_ navigate
+                _n_ rename symbol        _F_ format
+                _N_ rename file          _l_ tslint disable next line
 "
+    ("a" tide-nav)
     ("e" tide-project-errors)
     ("j" tide-jsdoc-template)
     ("d" tide-jump-to-definition)
     ("R" tide-refactor)
     ("n" tide-rename-symbol)
+    ("N" tide-rename-file)
     ("o" tide-organize-imports)
     ("r" tide-references)
-    ("S" tide-restart-server)
     ("f" tide-fix)
     ("F" tide-format)
+    ("l" tide-add-tslint-disable-next-line)
+    ("h" tide-documentation-at-point)
     ("v" tide-verify-setup)
-    ("h" tide-documentation-at-point))
+    ("S" tide-restart-server))
 
   (evil-define-key 'normal tide-mode-map (kbd "RET") #'/bindings/tide/hydra/body)
   (evil-define-key 'normal tide-mode-map (kbd "g r") #'tide-rename-symbol)
