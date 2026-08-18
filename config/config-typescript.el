adfe6aca3d876982520a6ffa7cb24dd10aa467aa
Bailey Ling <bling@live.ca>
2026-08-18 16:49:30 -0600

    modernize and improve lsp/eglot integration

--- config/config-typescript.el
+++ config/config-typescript.el
@@ -5,53 +5,23 @@
   :group 'dotemacs
   :prefix 'dotemacs-typescript)
 
-(defcustom dotemacs-typescript/tide-format-before-save t
-  "If non-nil, will auto format the buffer with tide prior to saving."
-  :group 'dotemacs-typescript
-  :type 'boolean)
-
 (defcustom dotemacs-typescript/engine
   'lsp
   "The engine to drive TypeScript."
   :type '(radio
-          (const :tag "tide" tide)
           (const :tag "eglot" eglot)
           (const :tag "lsp" lsp))
   :group 'dotemacs-typescript)
 
 
 
-(defun /typescript/setup ()
-  (cond
-   ((eq dotemacs-typescript/engine 'tide)
-    (require-package 'tide)
-    (setq tide-completion-detailed t)
-    (setq tide-completion-ignore-case t)
-    (setq tide-always-show-documentation t)
-    (when dotemacs-typescript/tide-format-before-save
-      (add-hook 'before-save-hook #'tide-format-before-save))
-
-    (tide-setup)
-    (tide-hl-identifier-mode t)
-    (eldoc-mode t))
-   ((eq dotemacs-typescript/engine 'eglot)
-    (/eglot/activate))
-   ((eq dotemacs-typescript/engine 'lsp)
-    (/lsp/activate))))
-
-(when (eq dotemacs-typescript/engine 'tide)
-  (after [tide evil]
-    (advice-add
-     'tide-jump-to-definition :before
-     (lambda (&rest _) (evil-set-jump)))))
-
 (when (fboundp 'typescript-ts-mode)
   (add-to-list 'auto-mode-alist '("\\.[mc]?ts\\'" . typescript-ts-mode))
-  (add-hook 'typescript-ts-mode-hook #'/typescript/setup))
+  (add-hook 'typescript-ts-mode-hook #'/utils/activate-lsp))
 
 (when (fboundp 'tsx-ts-mode)
   (add-to-list 'auto-mode-alist '("\\.[mc]?tsx\\'" . tsx-ts-mode))
-  (add-hook 'tsx-ts-mode-hook #'/typescript/setup))
+  (add-hook 'tsx-ts-mode-hook #'/utils/activate-lsp))
 
 (defun /typescript/generate-typings-for-css ()
   "Generates a Typescript type definition file for the current CSS file."
