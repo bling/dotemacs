da97a9d610559e7ba9c2401e0023fe7db3fd15b0
Bailey Ling <bling@live.ca>
2026-08-25 22:00:17 -0400

    s/hydra/transient

--- bindings/bindings.el
+++ bindings/bindings.el
@@ -1,15 +1,21 @@
 ;; -*- lexical-binding: t -*-
 
-(setq lv-use-separator t)
-(use-package hydra)
-
-
 (use-package transient
+  :demand t
   :init
   (setq transient-history-file (concat dotemacs-cache-directory "transient/history.el"))
   (setq transient-levels-file (concat dotemacs-cache-directory "transient/levels.el"))
-  (setq transient-values-file (concat dotemacs-cache-directory "transient/values.el")))
-
+  (setq transient-values-file (concat dotemacs-cache-directory "transient/values.el"))
+  (setq transient-enable-menu-navigation nil)
+  :config
+  (add-hook 'transient-setup-buffer-hook
+            (lambda ()
+              (setq-local cursor-in-non-selected-windows nil)
+              (setq-local cursor-type nil)))
+  (keymap-set transient-base-map "q" #'transient-quit-one)
+  (keymap-set transient-base-map "<escape>" #'transient-quit-one)
+  (keymap-set transient-sticky-map "q" #'transient-quit-seq)
+  (keymap-set transient-sticky-map "<escape>" #'transient-quit-seq))
 
 (defmacro /bindings/define-prefix-keys (keymap prefix &rest body)
   (declare (indent defun))
@@ -41,16 +47,16 @@
 (setq /bindings/normal-space-leader-map (make-sparse-keymap))
 (/bindings/define-prefix-keys /bindings/normal-space-leader-map "SPC"
   ("SPC" #'execute-extended-command "M-x")
-  ("t" #'/hydras/toggles/body "toggle...")
-  ("q" #'/hydras/quit/body "quit...")
-  ("e" #'/hydras/errors/body "errors...")
-  ("b" #'/hydras/buffers/body "buffers...")
-  ("j" #'/hydras/jumps/body "jump...")
-  ("f" #'/hydras/files/body "files...")
-  ("s" #'/hydras/search/body "search...")
-  ("l" #'/hydras/jumps/lambda-l-and-exit "lines(current)")
-  ("L" #'/hydras/jumps/lambda-L-and-exit "lines(all)")
-  ("o" #'/hydras/jumps/lambda-i-and-exit "outline")
+  ("t" #'/transients/toggles "toggle...")
+  ("q" #'/transients/quit "quit...")
+  ("e" #'/transients/errors "errors...")
+  ("b" #'/transients/buffers "buffers...")
+  ("j" #'/transients/jumps "jump...")
+  ("f" #'/transients/files "files...")
+  ("s" #'/transients/search "search...")
+  ("l" #'/transients/occur-current "lines(current)")
+  ("L" #'/transients/occur-all "lines(all)")
+  ("o" #'/transients/imenu "outline")
   ("v" vc-prefix-map)
   ("4" ctl-x-4-map)
   ("5" ctl-x-5-map "frames...")
@@ -68,16 +74,15 @@
 
 (after "magit-autoloads"
   (/bindings/define-prefix-keys /bindings/normal-space-leader-map "SPC"
-    ("g" #'/hydras/git/body "git...")))
-
+    ("g" #'/transients/git "git...")))
 
 (after "consult-autoloads"
   (/bindings/define-prefix-keys /bindings/normal-space-leader-map "SPC"
-    ("c" #'/hydras/consult/body "consult...")))
+    ("c" #'/transients/consult "consult...")))
 
 (after "helm-autoloads"
   (/bindings/define-prefix-keys /bindings/normal-space-leader-map "SPC"
-    ("h" #'/hydras/helm/body "helm...")))
+    ("h" #'/transients/helm "helm...")))
 
 (/bindings/define-prefix-keys /bindings/normal-space-leader-map "SPC"
   ("d" (bind
@@ -111,23 +116,11 @@
 
 ;; escape minibuffer
 (define-key minibuffer-local-map [escape] '/utils/minibuffer-keyboard-quit)
+(define-key minibuffer-local-map (kbd "C-w") 'backward-kill-word)
 (define-key minibuffer-local-ns-map [escape] '/utils/minibuffer-keyboard-quit)
 (define-key minibuffer-local-completion-map [escape] '/utils/minibuffer-keyboard-quit)
 (define-key minibuffer-local-must-match-map [escape] '/utils/minibuffer-keyboard-quit)
 (define-key minibuffer-local-isearch-map [escape] '/utils/minibuffer-keyboard-quit)
-
-(define-key minibuffer-local-map (kbd "C-w") 'backward-kill-word)
-
-
-
-
-(after "magit-autoloads"
-  (global-set-key (kbd "C-x g") #'/hydras/git/body))
-
-
-(global-set-key [f2] #'/explorer/toggle)
-(global-set-key [f3] #'/explorer/find-file)
-
 
 (after "multiple-cursors-autoloads"
   (global-set-key (kbd "C->") 'mc/mark-next-like-this)
@@ -135,50 +128,42 @@
   (global-unset-key (kbd "M-<down-mouse-1>"))
   (global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click))
 
-
-(after 'comint
-  (define-key comint-mode-map [up] 'comint-previous-input)
-  (define-key comint-mode-map [down] 'comint-next-input))
-
-
 (after "expand-region-autoloads"
   (global-set-key (kbd "C-=") 'er/expand-region))
 
+(after "magit-autoloads"
+  (/bindings/define-key (current-global-map) "C-c g" #'/transients/git "git..."))
+
+(after "helm-autoloads"
+  (/bindings/define-key (current-global-map) "C-c h" #'/transients/helm "helm..."))
 
 ;; mouse scrolling in terminal
 (unless (display-graphic-p)
   (global-set-key [mouse-4] (bind (scroll-down 1)))
   (global-set-key [mouse-5] (bind (scroll-up 1))))
 
+(after 'comint
+  (define-key comint-mode-map [up] 'comint-previous-input)
+  (define-key comint-mode-map [down] 'comint-next-input))
 
 (after 'compile
   (define-key compilation-mode-map (kbd "j") 'compilation-next-error)
   (define-key compilation-mode-map (kbd "k") 'compilation-previous-error))
 
-
-(after "helm-autoloads"
-  (/bindings/define-key (current-global-map) "C-c h" #'/hydras/helm/body "helm..."))
-
-
-
-
-(global-set-key [prior] 'previous-buffer)
-(global-set-key [next] 'next-buffer)
-
 (/bindings/define-keys (current-global-map)
   ("C-c c" #'org-capture)
   ("C-c a" #'org-agenda)
-  ("C-c l" #'org-store-link)
-  ("C-c s" #'/utils/goto-scratch-buffer)
-  ("C-c e" #'crux-eval-and-replace)
-  ("C-c t" #'/eshell/new-split))
+  ("C-c l" #'org-store-link))
 
 (/bindings/define-keys (current-global-map)
+  ("C-c s" #'/utils/goto-scratch-buffer)
+  ("C-c e" #'crux-eval-and-replace)
+  ("C-c t" #'/eshell/new-split)
   ("C-x c" #'calculator)
   ("C-x C" #'calendar)
   ("C-x C-b" #'ibuffer)
   ("C-x C-k" #'kill-current-buffer)
-  ("C-x n" #'/hydras/narrow/body)
+  ("C-x n" #'/transients/narrow "narrow...")
   ("C-x p" #'proced))
 
 (/bindings/define-keys (current-global-map)
@@ -187,23 +172,22 @@
   ("C-r"   #'isearch-backward-regexp)
   ("C-M-r" #'isearch-backward))
 
-
-(global-set-key (kbd "<f5>") (bind (profiler-start 'cpu+mem)))
-(global-set-key (kbd "<f6>") (bind (profiler-report) (profiler-stop)))
-
+(global-set-key [prior] 'previous-buffer)
+(global-set-key [next] 'next-buffer)
+(global-set-key [f2] #'/explorer/toggle)
+(global-set-key [f3] #'/explorer/find-file)
+(global-set-key [f5] (bind (profiler-start 'cpu+mem)))
+(global-set-key [f6] (bind (profiler-report) (profiler-stop)))
 
 ;; have no use for these default bindings
 (global-unset-key (kbd "C-x m"))
 
-
 (defun dotemacs/no-quit (&rest _)
   (message "Thou shall not quit!"))
-
 
 (global-set-key (kbd "C-x C-c") (bind (dotemacs/no-quit)))
 (after 'evil
   (advice-add 'evil-quit :override #'dotemacs/no-quit)
   (advice-add 'evil-quit-all :override #'dotemacs/no-quit))
-
 
 (provide 'config-bindings)
--- bindings/modes/lsp.el
+++ bindings/modes/lsp.el
@@ -1,44 +1,47 @@
 ;; -*- lexical-binding: t -*-
 
 (after 'lsp-mode
-  (defhydra /bindings/lsp/workspace/hydra (:exit t)
-    ("a" lsp-workspace-folders-add "add folder" :column "workspace")
-    ("r" lsp-workspace-folders-remove "remove folder")
-    ("s" lsp-workspace-folders-switch "switch folder"))
+  (transient-define-prefix /bindings/lsp/workspace/transient ()
+    ["workspace"
+     ("a" "add folder" lsp-workspace-folders-add)
+     ("r" "remove folder" lsp-workspace-folders-remove)
+     ("s" "switch folder" lsp-workspace-folders-switch)])
 
-  (defhydra /bindings/lsp/hydra (:exit t)
-    ("d" lsp-ui-peek-find-definitions "peek definition" :column "nav")
-    ("D" xref-find-definitions "xref definition")
-    ("r" lsp-ui-peek-find-references "peek references")
-    ("R" xref-find-references "xref references")
-    ("c" lsp-treemacs-call-hierarchy "call hierarchy")
-    ("T" lsp-treemacs-type-hierarchy "type hierarchy")
-    ("i" lsp-find-implementation "implementation")
-    ("t" lsp-find-type-definition "type definition")
-
-    ("n" lsp-rename "rename" :column "refactor")
-    ("o" lsp-organize-imports "organize imports")
-    ("f" lsp-ui-sideline-apply-code-actions "quick fix")
-    ("a" lsp-execute-code-action "code action")
-    ("=" lsp-format-buffer "format buffer")
-    ("F" lsp-format-region "format region")
-
-    ("h" lsp-describe-thing-at-point "doc at point" :column "info / errors")
-    ("g" lsp-ui-doc-glance "doc glance")
-    ("s" (if (eq dotemacs-switch-engine 'consult)
-             (call-interactively #'consult-lsp-symbols)
-           (call-interactively #'lsp-treemacs-symbols)) "symbols")
-    ("e" lsp-treemacs-errors-list "errors list")
-
-    ("S" lsp-restart-workspace "restart workspace" :column "server & toggles")
-    ("Q" lsp-shutdown-workspace "shutdown")
-    ("w" /bindings/lsp/workspace/hydra/body "folders")
-    ("H" lsp-inlay-hints-mode "inlay hints")
-    ("L" lsp-lens-mode "code lenses")
-    ("I" lsp-describe-session "session info"))
+  (transient-define-prefix /bindings/lsp/transient ()
+    [["nav"
+      ("d" "peek definition" lsp-ui-peek-find-definitions)
+      ("D" "xref definition" xref-find-definitions)
+      ("r" "peek references" lsp-ui-peek-find-references)
+      ("R" "xref references" xref-find-references)
+      ("c" "call hierarchy" lsp-treemacs-call-hierarchy)
+      ("T" "type hierarchy" lsp-treemacs-type-hierarchy)
+      ("i" "implementation" lsp-find-implementation)
+      ("t" "type definition" lsp-find-type-definition)]
+     ["refactor"
+      ("n" "rename" lsp-rename)
+      ("o" "organize imports" lsp-organize-imports)
+      ("f" "quick fix" lsp-ui-sideline-apply-code-actions)
+      ("a" "code action" lsp-execute-code-action)
+      ("=" "format buffer" lsp-format-buffer)
+      ("F" "format region" lsp-format-region)]
+     ["info / errors"
+      ("h" "doc at point" lsp-describe-thing-at-point)
+      ("g" "doc glance" lsp-ui-doc-glance)
+      ("s" "symbols" (lambda () (interactive)
+                       (if (eq dotemacs-switch-engine 'consult)
+                           (call-interactively #'consult-lsp-symbols)
+                         (call-interactively #'lsp-treemacs-symbols))))
+      ("e" "errors list" lsp-treemacs-errors-list)]
+     ["server & toggles"
+      ("S" "restart workspace" lsp-restart-workspace)
+      ("Q" "shutdown" lsp-shutdown-workspace)
+      ("w" "folders" /bindings/lsp/workspace/transient)
+      ("H" (lambda () (/transients/toggle-fmt "inlay hints" 'lsp-inlay-hints-mode)) lsp-inlay-hints-mode)
+      ("L" (lambda () (/transients/toggle-fmt "code lenses" 'lsp-lens-mode)) lsp-lens-mode)
+      ("I" "session info" lsp-describe-session)]])
 
   (after 'lsp-ui
-    (evil-define-key 'normal lsp-ui-mode-map (kbd "RET") #'/bindings/lsp/hydra/body)
+    (evil-define-key 'normal lsp-ui-mode-map (kbd "RET") #'/bindings/lsp/transient)
     (evil-define-key 'normal lsp-ui-mode-map (kbd "g r") #'lsp-rename)
     (evil-define-key 'normal lsp-ui-mode-map (kbd "g d") #'lsp-ui-peek-find-definitions)
     (evil-define-key 'normal lsp-ui-mode-map (kbd "K") #'lsp-describe-thing-at-point)
@@ -48,7 +51,7 @@
     (define-key lsp-ui-peek-mode-map (kbd "C-k") #'lsp-ui-peek--select-prev-file)
     (define-key lsp-ui-peek-mode-map (kbd "C-j") #'lsp-ui-peek--select-next-file))
 
-  (evil-define-key 'normal lsp-mode-map (kbd "RET") #'/bindings/lsp/hydra/body)
+  (evil-define-key 'normal lsp-mode-map (kbd "RET") #'/bindings/lsp/transient)
   (evil-define-key 'normal lsp-mode-map (kbd "g r") #'lsp-rename)
   (evil-define-key 'normal lsp-mode-map (kbd "g d") #'lsp-find-definition)
   (evil-define-key 'normal lsp-mode-map (kbd "K") #'lsp-describe-thing-at-point))
